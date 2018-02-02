b820a01a18c1e6f53cde871f8510f541b4c820e5
Luís Cabral <lcabral37@gmail.com>
2018-02-02 01:16:26 +0100

    rotate picture and search faces again

--- photo.py
+++ photo.py
@@ -8,7 +8,9 @@ import numpy
 import tkinter
 import time
 
+test= 50
 VIEW_SIZE = 400
+TOLERANCE = 0.3
 
 class Photo:
     "A object that loads and manipulates a given image"
@@ -20,12 +22,14 @@ class Photo:
         self.load()
 
     def load(self):
-        print_log("Loading photo '%s'" % self.filename)
         self._image = Image.open(self.filename)
 
         self._thumbnail = self._image.copy()
         self._thumbnail.thumbnail((VIEW_SIZE, VIEW_SIZE), Image.ANTIALIAS)
 
+        self.generateNumpy()
+
+    def generateNumpy(self):
         self._numpy = self._image.copy()
         self._numpy.thumbnail((self.parse_size, self.parse_size), Image.ANTIALIAS)
         self._numpy.convert("RGB")
@@ -44,14 +48,13 @@ class Photo:
             self._faces = face_recognition.face_locations(self._numpy)
         return self._faces
 
-    def display(self, window, grid, *show_faces):
+    def display(self, window, grid, *face):
         global tkpi
-        thumbnail = self._thumbnail.copy()
-        if show_faces:
-            faces = self.fix_ratio(self.faces)
-            thumbnail = self.draw_rectangles(thumbnail, faces)
-        tkpi[grid] = ImageTk.PhotoImage(thumbnail)
-        label_image = tkinter.Label(window, image=tkpi[grid], text=self.filename, compound=tkinter.BOTTOM)
+        if face:
+            face = self.fix_ratio([face])
+            self._thumbnail = self.draw_rectangles(self._thumbnail, face)
+        tkpi[grid] = ImageTk.PhotoImage(self._thumbnail)
+        label_image = tkinter.Label(window, image=tkpi[grid], text=self.filename, compound=tkinter.TOP)
         label_image.place(
             x=(VIEW_SIZE + 20) * grid + 10,
             y=10,
@@ -67,20 +70,32 @@ class Photo:
         return image
 
     def fix_ratio(self, areas, *ratio):
+        "Flatens and fixes the rectangles ratios list"
         result = []
         if len(areas):
             if not ratio:
                 ratio = VIEW_SIZE / self.parse_size
-                print("ratio: %f" % ratio)
-                print(areas[0])
-            for area in areas:
-                result.append(
-                    (int(ratio * area[0]), int(ratio * area[1]),
-                    int(ratio * area[2]), int(ratio * area[3]))
-                )
-        print(result)
+            for subareas in areas:
+                for area in subareas:
+                    result.append(
+                        (int(ratio * area[0]), int(ratio * area[1]),
+                        int(ratio * area[2]), int(ratio * area[3])))
         return result
 
+    def find(self, needles):
+        faces = face_recognition.face_locations(self.numpy)
+        if len(faces):
+            encodings = face_recognition.face_encodings(self.numpy, faces)
+            for idx, encoding in enumerate(encodings):
+                matches = face_recognition.compare_faces(needles, encoding, TOLERANCE)
+                if len(matches) :
+                    print("Found at %d" % idx)
+                    return faces[idx]
+
+    def rotate(self):
+        self._image = self._image.rotate(-90, expand=True)
+        self._thumbnail = self._thumbnail.rotate(-90, expand=True)
+        self.generateNumpy()
 def get_source():
     parser = argparse.ArgumentParser()
     parser.add_argument('image')
@@ -94,50 +109,44 @@ def get_source():
 def parse_folder():
     directory = "./source"
     counter = 0
-    print_log("Parsing haystach '%s'" % directory)
+    print("Parsing haystach '%s'" % directory)
     pathlist = Path(directory).glob("**/*.jpg")
 
     for filename in pathlist:
         counter += 1
-        if counter < 20:
-            print_log("%05d: %s" % (counter, filename))
+        if counter < test:
+            print("%05d: %s" % (counter, filename))
             try:
                 photo = Photo(filename)
                 photo.display(root, 1)
-                encodings = face_recognition.face_encodings(photo.numpy)
-                if encodings and face_recognition.compare_faces([known_face], encodings[0]):
-                    photo.display(root, 2, True)
+                found = photo.find(known_faces)
+                if not found:
+                    photo.rotate()
+                    found = photo.find(known_faces)
+                if found:
+                    photo.display(root, 2, found)
             except RuntimeError:
-                print_log("Failed %s" % filename)
+                print("Failed %s" % filename)
             except OSError:
-                print_log("Failed %s" % filename)
+                print("Failed %s" % filename)
         else:
             break
-    print_log("Finished analysing %d images" % counter)
+    print("Finished analysing %d images" % counter)
     time.sleep(5)
     root.destroy()
-
-def print_log(text):
-    if textBox and text:
-        textBox.insert(tkinter.END, text)
-        textBox.insert(tkinter.END, "\n")
-        root.update()
-    print(text)
 
 tkpi = [0,1,2]
 root = tkinter.Tk()
 root.geometry("+%d+%d" % (300, 300))
-root.geometry("%dx%d" % ((VIEW_SIZE + 20) * 3, VIEW_SIZE + 20 + 100))
+root.geometry("%dx%d" % ((VIEW_SIZE + 20) * 3, VIEW_SIZE + 20 + VIEW_SIZE))
 
-textBox = tkinter.Text(root)
-textBox.place(x=10, y=VIEW_SIZE + 20, height=50, width=VIEW_SIZE * 3)
-
-print_log("Parsing needle")
+print("Parsing needle")
 
 known_photo = Photo(get_source())
-known_photo.display(root, 0)
-
-known_face = face_recognition.face_encodings(known_photo.numpy)
-known_photo.display(root, 0, True)
+known_photo.display(root, 1)
+known_faces  = [face_recognition.face_encodings(known_photo.numpy)]
+location = face_recognition.face_locations(known_photo.numpy)
+print("known location: %s" % str(location))
+known_photo.display(root, 0, location[0])
 root.after(500, parse_folder)
 root.mainloop()
